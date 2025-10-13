b6571b9e8065f36e0234274149b1adf4af66176f
Vivekanand Alampally <vivek4348@gmail.com>
2025-10-12 20:06:31 -0400

    Updated

---
+++ Demos/Tailwind/tailwind-sandbox-done/tailwind.config.js
@@ -0,0 +1,12 @@
+    // tailwind.config.js
+    module.exports = {
+      content: [
+        // Specify the files where you use Tailwind classes
+        // For a simple HTML project, you might include your HTML files
+        "./*.html",
+      ],
+      theme: {
+        extend: {},
+      },
+      plugins: [],
+    };
